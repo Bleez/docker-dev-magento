c9ffa3f7a97622f9c1a6641c6e195a61c2f1097d
Bruno Viana <brunoviana@gmail.com>
2020-04-16 23:03:40 -0300

    Ajustes env.sample.php

--- extras/env.sample.php
+++ extras/env.sample.php
@@ -10,7 +10,7 @@ return array(
         ),
     'session' =>
         array(
-            'save' => '<projeto>-redis',
+            'save' => 'redis',
             'redis' =>
                 array(
 
